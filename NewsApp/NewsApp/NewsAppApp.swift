d43282332f4a0412944e4235701d5d49aa3ce3a2
elliott <elliott>
2024-10-02 21:08:22 +0300

    no message

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Мадина Валиева on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
